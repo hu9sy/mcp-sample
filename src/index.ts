1bbcf5d0f6a51108cfa4ebd91859522029bd7af9
hu9sy <awesome4dev@gmail.com>
2025-08-23 11:24:35 +0900

    update: template index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { name, version } from '../package.json';
+import { logger } from './utils/logger.js';
+
+const server = new McpServer({
+  name: name,
+  version: version,
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  logger.info("MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  logger.error("Fatal error in main()", error);
+  process.exit(1);
+});
