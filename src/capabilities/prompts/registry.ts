deb2cd800d6c97d9d6a1d26e5adba8cc3e836897
hu9sy <awesome4dev@gmail.com>
2025-08-24 22:39:06 +0900

    remove: unused prompts registry type definition

--- src/capabilities/prompts/registry.ts
+++
@@ -1,9 +0,0 @@
-import { z, ZodType } from 'zod';
-
-export type Prompt<TSchema extends ZodType = ZodType> = {
-    name: string;
-    title: string;
-    description: string;
-    schema: TSchema;
-    handler: (input: z.infer<TSchema>) => Promise<any>;
-};
